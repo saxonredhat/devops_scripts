1ee9812efe495629960acd6f30d479b189fc27d4
saxon.redhat <saxon.redhat@gmail.com>
2019-08-15 10:22:54 +0800

    添加重试次数，过滤因为网络偶尔抖动而导致的误告警

--- check_web.sh
+++ check_web.sh
@@ -1,22 +1,37 @@
 project_dir=/data/deploy/scripts
 date_subfix=$(date "+%Y-%m-%d_%H:%M:%S")
-check_url=${1:-"https://example.com/api"}
+#check_url=${1:-"https://pan.codemax.org.cn:6443/login"}
+check_url=${1:-"https://xxxxx.com/login"}
 error_msg=${2:-"[告警通知]网盘异常，请及时处理！"}
 ok_msg=${3:-"[恢复通知]网盘恢复正常！"}
 error_time_file=${4:-"$project_dir/netdisk_error_time.txt"}
-request_method=${5:-"GET"}
-ret_code=$(curl -X $request_method -I -m 4 -o /dev/null -s -w %{http_code} $check_url)
+retries=10
+intervals=3
+dingding_token="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 [ ! -e $error_time_file ]&&echo -n 0 >$error_time_file
 error_time=$(cat $error_time_file)
-dingding_token="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#ret_code=$(curl -I -m 4 -o /dev/null -s -w %{http_code} $check_url)
+#对监控API多次尝试
+retry_counts=0
+while [ $retry_counts -lt $retries ];do
+    ret_code=$(curl -I -m 4 -o /dev/null -s -w %{http_code} $check_url)
+    if [ x"$ret_code" == x"200" ];then
+        break
+    fi
+    sleep $intervals
+    retry_counts=$(expr $retry_counts + 1)
+    echo "共${retries}次，重试第${retry_counts}次..."
+done
+
 function dingding(){
     token=$1
     msg=$2
     curl https://oapi.dingtalk.com/robot/send?access_token=$token \
        -H 'Content-Type: application/json' \
-       -d '{"msgtype": "text", 
-        "text": {
-             "content": "'$msg'"
+       -d '{"msgtype": "markdown", 
+        "markdown": {
+	     "title": "消息",
+             "text": "'$msg'"
         }
     }'
 
@@ -32,9 +47,9 @@ if [ x"$ret_code" == x"200" ];then
 else
     echo "error"
     #判断里面的值为0，发送钉钉
-    if [ $error_time -eq 0 ];then
-	dingding $dingding_token ${date_subfix}-$error_msg
-    fi
     #把error_time+1
+    if [ $error_time -eq 0 ];then
+	dingding $dingding_token $date_subfix-$error_msg
+    fi
     echo -n $(expr $error_time + 1) >$error_time_file
 fi
